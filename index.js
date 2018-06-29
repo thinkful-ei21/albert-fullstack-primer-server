a43d3329c2860ba090dc2d3945c7c37ad5eb5931
Albert Sare <thesundaymailman@gmail.com>
2018-06-29 09:45:16 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const { PORT, CLIENT_ORIGIN } = require('./config');
+const { dbConnect } = require('./db-mongoose');
+// const {dbConnect} = require('./db-knex');
+
+const app = express();
+
+app.use(morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+  skip: (req, res) => process.env.NODE_ENV === 'test'
+}));
+
+app.use(cors({
+  origin: CLIENT_ORIGIN
+}));
+
+app.use(express.json());
+
+
+const data = [
+  'Bath Blue',
+  'Barkham Blue',
+  'Buxton Blue',
+  'Cheshire Blue',
+  'Devon Blue',
+  'Dorset Blue Vinney',
+  'Dovedale',
+  'Exmoor Blue',
+  'Harbourne Blue',
+  'Lanark Blue',
+  'Lymeswold',
+  'Oxford Blue',
+  'Shropshire Blue',
+  'Stichelton',
+  'Stilton',
+  'Blue Wensleydale',
+  'Yorkshire Blue',
+  'blah blah blah'
+];
+
+
+app.get('/api/cheese', (req, res, next) => {
+  res.json(data);
+});
+
+function runServer(port = PORT) {
+  const server = app
+    .listen(port, () => {
+      console.info(`App listening on port ${server.address().port}`);
+    })
+    .on('error', err => {
+      console.error('Express failed to start');
+      console.error(err);
+    });
+}
+
+if (require.main === module) {
+  dbConnect();
+  runServer();
+}
+
+module.exports = { app };
